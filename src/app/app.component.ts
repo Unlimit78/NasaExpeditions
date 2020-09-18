be2a00c9353d6b1de5b1aacd7aa2fa2e14a3cfe4
Unlimit78 <taras_ilyashchuk161@ukr.net>
2020-09-18 21:18:35 +0300

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Photo,} from './Interfaces';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NasaExpeditions';
+  public list: Photo[];
+  public rover: string;
+  public camera: string;
+  public sol: number;
+  public message: string;
+  private httpClient: HttpClient;
+  public rovers: {};
+  public cameras: {};
+  public limit: number;
+  public loaded: boolean;
+
+
+
+
+  httpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json'
+  });
+
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
+    this.rovers = ['Opportunity', 'Curiosity', 'Spirit'];
+    this.cameras = [
+      {name: 'FHAZ', full: 'Front Hazard Avoidance Camera'},
+      {name: 'RHAZ', full: 'Rear Hazard Avoidance Camera'},
+      {name: 'MAST', full: 'Mast Camera'},
+      {name: 'CHEMCAM', full: 'Chemistry and Camera Complex'},
+      {name: 'MAHLI', full: 'Mars Hand Lens Imager'},
+      {name: 'MARDI', full: 'Mars Descent Imager'},
+      {name: 'NAVCAM', full: 'Navigation Camera'},
+      {name: 'PANCAM', full: 'Panoramic Camera'},
+      {name: 'MINITES', full: 'Miniature Thermal Emission Spectrometer (Mini-TES)'}
+
+    ];
+    this.limit = 20;
+  }
+
+
+
+
+  getData(): void{
+    this.httpClient.get('https://api.nasa.gov/mars-photos/api/v1/rovers/' + this.rover + '/photos?sol=' + this.sol + '&camera=' + this.camera + '&api_key=KsfTtYjvdZgyTy72gsFXPDJcgERKaaH5LPgDCizn',{headers: this.httpHeaders}).subscribe(list => {
+
+      this.list = list['photos'];
+      if (this.list.length === 0){
+        this.message = 'There is no photos';
+      }
+      else{
+        this.message = 'There is ' + this.list.length + ' photos';
+      }
+      this.loaded = true;
+      console.log(this.list);
+
+    });
+  }
+  loadMore(): void{
+      this.limit += 20;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatOptionModule} from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatButtonModule,
+    MatSelectModule,
+
+
+
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
